89e24a09664736ae19930665ba16955e86e18eb0
profjaquetafine <88409934+profjaquetafine@users.noreply.github.com>
2022-11-22 17:23:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,20 @@
 document.querySelector('.calcular').addEventListener('click',()=>{
     var n1= parseFloat( document.querySelector('.nota1').value)
     var n2 = parseFloat( document.querySelector('.nota2').value)
-    
-    var soma = n1 + n2
+    var n3= parseFloat( document.querySelector('.nota3').value)
+    var n4 = parseFloat( document.querySelector('.nota4').value)
+    var soma = n1 + n2 + n3 + n4
+var media = soma/4
 
-    document.querySelector('.resultado').innerHTML = soma
+if(media < 5){
+    
+    document.querySelector('.resultado').innerHTML = `sua média é: ${media}.<br> E você foi Reprovado`
+}else if(media < 7){
+    document.querySelector('.resultado').innerHTML = `sua média é: ${media}.<br> E você está de recuperação`
+}else{
+    document.querySelector('.resultado').innerHTML = `sua média é: ${media}. <br>E você foi Aprovado`
+}
+   
+
+
 })
